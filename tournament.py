56ec0a26eff7e64060eba751661ebdd1e517a4ca
TickingFeather <49458013+TickingFeather@users.noreply.github.com>
2019-04-10 09:15:11 +0200

    Supprimé compteur de matchs inutile
    - Suppression de cpt() car retrouvable par len(matchlist)
    - Nettoyage de qques commentaires de test inutiles

--- tournament.py
+++ tournament.py
@@ -7,8 +7,6 @@ def main(n,short): # https://fr.wikipedia.org/wiki/Table_de_Berger
     #init variables
     #global short  # raccourcir en enlevant les matchs doublons
     #short=s       #
-    global cpt      # compteur de matchs
-    cpt=0
     global matchlist# liste des matchs sous forme de tuples (ronde,joueur1,joueur2)
     matchlist=[]
     
@@ -16,8 +14,6 @@ def main(n,short): # https://fr.wikipedia.org/wiki/Table_de_Berger
     for r in range(1,n): #toutes les rondes de 1 à n-1
         for a in range(1,n+1): #tous joueurs a de 1 à n
             for b in range(1,n+1): #tous joueurs b de 1 à n
-                #a=1
-                #b=2
                 if b!=n:
                     if a+b-1<n:
                         if r==a+b-1:
@@ -32,19 +28,16 @@ def main(n,short): # https://fr.wikipedia.org/wiki/Table_de_Berger
                     else: # 2a>n
                         if r==2*a-n:
                             matchCreate(a,b,r,short)
-    #print(cpt)
     return matchlist
+    
 
 def matchCreate(a,b,r,short):
-    global cpt
     if a!=b: # enlever les matchs contre soi-même
         if short==True: # si short est True, on test le résultat de duplicateMatch
             if duplicateMatch(a,b)!=True:
-                cpt=cpt+1
                 matchlist.append((r,a,b))
                 #print('Ronde {} : {} VS {}'.format(r,a,b))
         else:       #sinon on ignore duplicateMatch
-                cpt=cpt+1
                 matchlist.append((r,a,b))
                 #print('Ronde {} : {} VS {}'.format(r,a,b))
 
@@ -57,6 +50,5 @@ def duplicateMatch(p1,p2): #fonction vérifiant les matchs doublons, matchlist s
             return True
 
 if __name__ == '__main__':
-    
     import sys
     sys.exit(main(6,True))
