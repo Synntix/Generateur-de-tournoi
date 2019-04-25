ab29323faf1347e08f6b4d718431658356e9a6e0
TickingFeather <49458013+TickingFeather@users.noreply.github.com>
2019-04-25 20:15:46 +0200

    Réécriture majeure /!\ Changements arguments fonctions (v. desc. avancée)
    - main() obsolète, utiliser getMatchList(nbplayers,shortmode) uniquement
    - argument shortmode optionnel, défaut=True
    - ajout argument optionnel à getMatchlist pour choix méthode : ruban (False, par défaut) ou Berger (True)
    - ajout méthode du ruban, beaucoup plus rapide que Berger si grand nombre joueurs
    - optimisation mineure de Berger (ajout break)
    - commentaires

--- tournament.py
+++ tournament.py
@@ -1,35 +1,54 @@
-import random
 
+#def main(n,short): # https://fr.wikipedia.org/wiki/Table_de_Berger
+#    return getMatchList(n,short)
 
-
-def main(n,short): # https://fr.wikipedia.org/wiki/Table_de_Berger
-
-    #init variables
-    #global short  # raccourcir en enlevant les matchs doublons
-    #short=s       #
-    global matchlist# liste des matchs sous forme de tuples (ronde,joueur1,joueur2)
+def getMatchList(n,short=True,Berger=False):
+    global matchlist # Init liste des matchs sous forme de tuples (ronde,joueur1,joueur2), globale
     matchlist=[]
-    
-    
+    if Berger==True: # choix méhode Berger ou ruban
+        return methodeBerger(n,short)
+    else:
+        return methodeRuban(n,short)
+
+def methodeBerger(n,short):
+
     for r in range(1,n): #toutes les rondes de 1 à n-1
         for a in range(1,n+1): #tous joueurs a de 1 à n
             for b in range(1,n+1): #tous joueurs b de 1 à n
-                if b!=n:
+                if b!=n and a!=n:
                     if a+b-1<n:
                         if r==a+b-1:
                             matchCreate(a,b,r,short)
+                            break
                     else: # a+b-1>=n
                         if r==a+b-n:
                             matchCreate(a,b,r,short)
-                else: # b=n
+                            break
+                else:
                     if 2*a<=n:
                         if r==2*a-1:
                             matchCreate(a,b,r,short)
+                            break
                     else: # 2a>n
                         if r==2*a-n:
                             matchCreate(a,b,r,short)
+                            break
     return matchlist
-    
+def methodeRuban(n,short):
+    if n%2!=0:
+        return [] # Sécurité: vérifier si nombre joueurs est bien pair
+    Rb=[i for i in range(2,n+1)] # générer ruban de joueurs sauf le 1
+    h=0
+    b=int((n/2)-1) # initialiser les pointeurs
+
+    for r in range(1,n):
+        matchCreate(1,Rb[int(b+n/2-1)%(n-1)],r,short) # Cas du 1
+        for i in range(0,int(n/2-2)+1):
+            matchCreate(Rb[(h+i)%(n-1)],Rb[int(b+n/2-2-i)%(n-1)],r,short)
+        h=(h-1)%(n-1)
+        b=(b-1)%(n-1)
+    return matchlist
+
 
 def matchCreate(a,b,r,short):
     if a!=b: # enlever les matchs contre soi-même
@@ -41,8 +60,6 @@ def matchCreate(a,b,r,short):
                 matchlist.append((r,a,b))
                 #print('Ronde {} : {} VS {}'.format(r,a,b))
 
-def getMatchList():
-    return matchlist
 
 def duplicateMatch(p1,p2): #fonction vérifiant les matchs doublons, matchlist sert d'historique
     for i in matchlist:
@@ -51,4 +68,4 @@ def duplicateMatch(p1,p2): #fonction vérifiant les matchs doublons, matchlist s
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(6,True))
+    sys.exit(getMatchList(14,False,True))
