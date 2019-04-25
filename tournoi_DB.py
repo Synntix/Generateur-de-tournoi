ec2e2356462442aade2c22dc365f42ed9c73d48a
Drusstroyer <49471160+Drusstroyer@users.noreply.github.com>
2019-04-25 09:23:29 +0200

    Add files via upload
    alors ya bcp de nouvelle fonction pour assurer un fonctionnement malgré des changements ailleurs donc juste remettre les bonnes variables et me mp si il faut rajouter des nouvelles fonctions

--- tournoi_DB.py
+++ tournoi_DB.py
@@ -1,16 +1,67 @@
 import sqlite3
 import isnflask
 
-    
 
-def main():
+#def main():
+
+  #  openDB()
+  #  createTables()
+
+   # testPlayers = ["Alain","Julien", "Alex", "Eliott"]
+    
+   # createPlayers(testPlayers)
+
+  #  resPlayer = getPlayers()
+    
+  #  for i in range(len(resPlayer)):
+   #     print(resPlayer[i])
+
+   # testIds = [4,2]
+    
+   # resPseudo= getPseudo(testIds)
+    
+    #for i in range(len(resPseudo)):
+       # print(resPseudo[i])
+
+    #closeDB()
+
+# SCRUD
+# SEARCH
+
+
+def getPlayers():
+
+    players = curseur.execute("SELECT id FROM scores ").fetchall()
+
+    return players
+
+
+def getPseudo(pIds):
+
+    # tupleIds = ""
+    # for i in range(len(pIds)):
+    #     tupleIds += str(pIds[i]) + ","
+
+    questionmarks = '?' * len(pIds)
+    formatted_query = 'SELECT pseudo FROM scores WHERE id in ({})'.format(','.join(questionmarks))
+    pseudos = curseur.execute(formatted_query, pIds).fetchall()
+
+    return pseudos
+
+# CREATE
+def openDB():
     global connexion
-    global curseur
     connexion = sqlite3.connect("tournoi.sqlite3")
-    
-    
+
+def closeDB():
+    connexion.close()
+
+
+def createTables():
+    global curseur
+
     curseur = connexion.cursor()
-    
+
     curseur.executescript('''DROP TABLE IF EXISTS scores;
     
         CREATE TABLE IF NOT EXISTS scores(
@@ -18,44 +69,35 @@ def main():
         pseudo TEXT,
         victoire INTEGER,
         defaite INTEGER);''')
-    
+
     curseur.executescript('''DROP TABLE IF EXISTS match;
     
         CREATE TABLE IF NOT EXISTS match(
         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-        round INTEGER?
+        round INTEGER,
         joueur1 INTEGER,
         joueur2 INTEGER,
         vainqueur INTEGER);''')
 
-   
-def getid(Players):
 
-    for i in range(len(Players)) :
-        Players[i]=(Players[i],)
-    
-    #Exécutions multiples
-    curseur.executemany('INSERT INTO scores (pseudo) VALUES (?)', Players)
-    
-    connexion.commit()  #Ne pas oublier de valider les modifications
-    
-    id=curseur.execute("SELECT id FROM scores ").fetchall()
-    
-    return id
-        
-        
-def getPseudo(result):
-    
-    Pseudo=curseur.execute('SELECT pseudo FROM scores WHERE id = ?',result).fetchall()
-    
-    return Pseudo
+def createPlayers(pPlayers):
 
-    connexion.close()
-    
+    tuplePlayers = []
+    for i in range(len(pPlayers)):
+        tuplePlayers.append((pPlayers[i],))
+
+    curseur.executemany('INSERT INTO scores (pseudo) VALUES (?)', tuplePlayers)
+
+    connexion.commit()
+
+# READ
+
+# UPDATE
+
+# DELETE
+
+
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
-
-
-
-
+#    sys.exit(main(sys.argv))
+    main()
