7a4751121fbb9ad4b0ca84cd9c70d9d6586ea0b1
Drusstroyer <49471160+Drusstroyer@users.noreply.github.com>
2019-05-01 12:17:10 +0200

    Add files via upload

--- tournoi_DB.py
+++ tournoi_DB.py
@@ -1,29 +1,40 @@
 import sqlite3
 import isnflask
+import tournament
+
+def main():
 
 
-#def main():
+    #openDB()
+    #createTables()
 
-  #  openDB()
-  #  createTables()
+    #testPlayers = ["Alain","Julien", "Alex", "Eliott"]
 
-   # testPlayers = ["Alain","Julien", "Alex", "Eliott"]
-    
-   # createPlayers(testPlayers)
+    #createPlayers(testPlayers)
 
-  #  resPlayer = getPlayers()
-    
-  #  for i in range(len(resPlayer)):
-   #     print(resPlayer[i])
+    #resPlayer = getPlayers()
 
-   # testIds = [4,2]
-    
-   # resPseudo= getPseudo(testIds)
-    
+    #for i in range(len(resPlayer)):
+        #print(resPlayer[i])
+
+    #testIds = [4]
+
+    #resPseudo= getPseudo(testIds)
+
     #for i in range(len(resPseudo)):
-       # print(resPseudo[i])
+        #print(resPseudo[i])
+    #n = 4
+    #listmatch = tournament.getMatchList(n)
+    #print(listmatch)
+    #creatematch(listmatch)
+    #vainq =isnflask.results()
+    #createVictoire(vainq)
+    #testVictoire = [2,3,4]
 
-    #closeDB()
+    #upVictoire(testVictoire)
+
+
+    closeDB()
 
 # SCRUD
 # SEARCH
@@ -31,7 +42,7 @@ import isnflask
 
 def getPlayers():
 
-    players = curseur.execute("SELECT id FROM scores ").fetchall()
+    players = curseur.execute("SELECT id FROM joueurs ").fetchall()
 
     return players
 
@@ -43,7 +54,7 @@ def getPseudo(pIds):
     #     tupleIds += str(pIds[i]) + ","
 
     questionmarks = '?' * len(pIds)
-    formatted_query = 'SELECT pseudo FROM scores WHERE id in ({})'.format(','.join(questionmarks))
+    formatted_query = 'SELECT pseudo FROM joueurs WHERE id in ({})'.format(','.join(questionmarks))
     pseudos = curseur.execute(formatted_query, pIds).fetchall()
 
     return pseudos
@@ -62,16 +73,17 @@ def createTables():
 
     curseur = connexion.cursor()
 
-    curseur.executescript('''DROP TABLE IF EXISTS scores;
-    
-        CREATE TABLE IF NOT EXISTS scores(
+    curseur.executescript('''DROP TABLE IF EXISTS joueurs;
+
+        CREATE TABLE IF NOT EXISTS joueurs(
         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
         pseudo TEXT,
         victoire INTEGER,
-        defaite INTEGER);''')
+        defaite INTEGER,
+        egalite INTEGER);''')
 
     curseur.executescript('''DROP TABLE IF EXISTS match;
-    
+
         CREATE TABLE IF NOT EXISTS match(
         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
         round INTEGER,
@@ -86,18 +98,28 @@ def createPlayers(pPlayers):
     for i in range(len(pPlayers)):
         tuplePlayers.append((pPlayers[i],))
 
-    curseur.executemany('INSERT INTO scores (pseudo) VALUES (?)', tuplePlayers)
+    curseur.executemany('INSERT INTO joueurs (pseudo) VALUES (?)', tuplePlayers)
 
     connexion.commit()
 
+def creatematch(listmatch):
+    curseur.executemany('INSERT INTO match (round,joueur1,joueur2) VALUES (?,?,?)',listmatch)
+    connexion.commit()
+
+##def createVictoire(vainq):
+    ##curseur.executemany('INSERT INTO match (vainqueur) VALUES (?)',vainq)
+    ##connexion.commit()
 # READ
 
 # UPDATE
+#def upVictoire(pVictoire):
+    #curseur.execute('UPDATE joueurs SET victoire = (?) WHERE id=1 ',pVictoire)
 
+    #connexion.commit()
 # DELETE
 
 
 if __name__ == '__main__':
     import sys
-#    sys.exit(main(sys.argv))
-    main()
+    sys.exit()
+main()
