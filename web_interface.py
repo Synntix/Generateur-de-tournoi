068ec3f8d9a393c329950ecfb5329b960c4065d8
Alexandre <alexandre.legendre38@gmail.com>
2019-06-02 19:10:17 +0200

    /!\ Gros changements : implémentation du mode score par match, getClassement à adapter /!\

--- web_interface.py
+++ web_interface.py
@@ -50,19 +50,15 @@ def player_entry():
     #On récupère les réponses des formulaires de la page d'accueil
     session['Nbr_player'] = int(request.form['nbr_player'])
     session['Type_tournoi']=request.form['type_tournoi']
-    session['Pts_win']=int(request.form['pts_win'])
-    session['Pts_draw']=int(request.form['pts_draw'])
-    session['Pts_lose']=int(request.form['pts_lose'])
+    session['Mode_points']=request.form['mode_points']
+
 
     if debug==True:
         print("Nombre de joueurs : {}".format(session['Nbr_player']))
         print("Type de tournoi : {}".format(session['Type_tournoi']))
-        print("Nombre de points par match gagné : {}".format(session['Pts_win']))
-        print("Nombre de points par égalité : {}".format(session['Pts_draw']))
-        print("Nombre de points par match perdu : {}".format(session['Pts_lose']))
 
     #On utilise le template page2.html
-    return render_template('page2.html.j2' , nbr_player=session['Nbr_player'], type_tournoi=session['Type_tournoi'])
+    return render_template('page2.html.j2' , nbr_player=session['Nbr_player'], type_tournoi=session['Type_tournoi'], mode_points=session['Mode_points'])
 
 
 
@@ -76,6 +72,15 @@ def display():
         session['Players'].append(request.form['pseudo{}'.format(i)])
     if debug==True:
         print("Liste des joueurs :\n{}".format(session['Players']))
+
+    if session['Mode_points']=="TOR":
+        session['Pts_draw']=int(request.form['pts_draw'])
+        session['Pts_lose']=int(request.form['pts_lose'])
+        session['Pts_win']=int(request.form['pts_win'])
+        if debug==True:
+            print("Nombre de points par match gagné : {}".format(session['Pts_win']))
+            print("Nombre de points par égalité : {}".format(session['Pts_draw']))
+            print("Nombre de points par match perdu : {}".format(session['Pts_lose']))
 
     #On récupère le choix sur la longueur du tournoi et on en fait un booléen
     Extended=request.form['shortcheckbox']
@@ -96,6 +101,7 @@ def display():
         print("Barrages = {}".format(Barrages))
 
     #On récupère la liste des matchs
+    #Matchlist est de la forme [(numéro_round,id_j1,id_j2),...]
     Matchlist=tournament.getMatchList(session['Nbr_player'],Extended,False,debug_algo)
     session['Matchlist']=Matchlist
     if debug==True:
@@ -109,6 +115,7 @@ def display():
     tournoi_DB.createPlayers(session['Players'])
 
     #On crée une liste des matchs avec le pseudo des joueurs au lieu de leur IDs
+    #Matchlist_pseudo est de la forme [(numéro_round,pseudo_j1,pseudo_j2),...]
     Matchlist_pseudo=deepcopy(Matchlist)
     for i in range(len(Matchlist_pseudo)):
         Matchlist_pseudo[i]=list(Matchlist_pseudo[i])
@@ -119,22 +126,49 @@ def display():
         print("Liste des matchs par pseudo : \n{}".format(Matchlist_pseudo))
 
     #On utilise le template display.html
-    return render_template('display.html.j2' , players=session['Players'] ,nbr_player=session['Nbr_player'], type_tournoi=session['Type_tournoi'], matchlist=Matchlist, matchlist_pseudo=Matchlist_pseudo, nbr_matchs=session['Nbr_matchs'])
+    return render_template('display.html.j2' , players=session['Players'] ,nbr_player=session['Nbr_player'], type_tournoi=session['Type_tournoi'], matchlist=Matchlist, matchlist_pseudo=Matchlist_pseudo, nbr_matchs=session['Nbr_matchs'], mode_points=session['Mode_points'])
 
 
 
 
 @app.route('/results/', methods=['POST'])
 def results():
-    results=[]
-    for i in range(1,session['Nbr_matchs']+1) :
-        #On récupère l'id des joueurs qui ont gagné pour les mettre dans la liste results
-        results.append(int(request.form['match{}'.format(i)]))
-    if debug==True:
-        print("Liste des IDs des gagnants (0 = égalité) : \n{}".format(results))
+    if session['Mode_points'] == "score":
+        #score_per_match est de la forme [(score j1 match1,score j2 match1),(score j1 match2,score j2 match2),...]
+        score_per_match=[]
+        for i in range(1,session['Nbr_matchs']+1) :
+            #On récupère le score du match pour le mettre dans la liste score_per_match
+            tuple_score=(request.form['score_j1_match{}'.format(i)]),(request.form['score_j2_match{}'.format(i)])
+            score_per_match.append(tuple_score)
+            if debug==True:
+                print("Liste des scores : \n{}".format(score_per_match))
+
+        results=[]
+        for i in range(session['Nbr_matchs']) :
+            #On compare les scores des joueurs pour déduire qui a gagné pour le mettre dans la liste results
+            if score_per_match[i][0]>score_per_match[i][1]:
+                results.append(session['Matchlist'][i][1])
+            elif score_per_match[i][0]<score_per_match[i][1]:
+                results.append(session['Matchlist'][i][2])
+            elif score_per_match[i][0]==score_per_match[i][1]:
+                results.append(0)
+                if debug==True:
+                    print("Liste des IDs des gagnants (0 = égalité) : \n{}".format(results))
+
+    elif session['Mode_points'] == "TOR":
+        results=[]
+        for i in range(1,session['Nbr_matchs']+1) :
+            #On récupère l'id des joueurs qui ont gagné pour les mettre dans la liste results
+            results.append(int(request.form['match{}'.format(i)]))
+        if debug==True:
+            print("Liste des IDs des gagnants (0 = égalité) : \n{}".format(results))
 
     #On récupère le classement et le convertit en classement_pseudo qui contient les pseudos
-    session['Classement_pseudo']=tournament.getClassement(session['Nbr_player'],session['Matchlist'],results,session['Pts_win'],session['Pts_draw'],session['Pts_lose'],debug_algo)
+    if session['Mode_points'] == "TOR":
+        session['Classement_pseudo']=tournament.getClassement(session['Nbr_player'],session['Matchlist'],results,session['Pts_win'],session['Pts_draw'],session['Pts_lose'],debug_algo)
+    elif session['Mode_points'] == "score":
+        #/!\ À CHANGER DÈS QUE ELIOTT A ADAPTÉ SA FONCTION /!\
+        session['Classement_pseudo']=tournament.getClassement(session['Nbr_player'],session['Matchlist'],results,5,1,-2,debug_algo)
     for i in range(len(session['Classement_pseudo'])):
         session['Classement_pseudo'][i]=list(session['Classement_pseudo'][i])
     for i in range(len(session['Classement_pseudo'])):
