0a625fc46f7a4d736fecdfdd46b3158948d344ca
TickingFeather <clanking.feather@gmail.com>
2019-06-05 16:58:08 +0200

    Prise en compte des scores per match dans le calcul (ça a l'air de marcher)
    - Ajout argument scoremode (false par défaut) à getClassement pour 
    utiliser le mode scores. L'argument results doit être une liste de 
    listes [pts j1,pts j2] pour chaque match, et kw/kd/kl les bonus par 
    évènement
    - SimulateRandom adapté en conséquence
    - Changements debug, commentaire et nettoyage à venir

--- tournament.py
+++ tournament.py
@@ -1,4 +1,3 @@
-import tournoi_DB
 
 def getMatchList(n,extended=False,Berger=False,debug_algo=False): # Fonction principale, retourne liste des matchs pour n joueurs
     global debug
@@ -13,7 +12,7 @@ def getMatchList(n,extended=False,Berger=False,debug_algo=False): # Fonction pri
     else:
         Bye=False
     if debug:
-        print("#ALGO# Nb joueurs = {} \n#ALGO# Bye = {}".format(n,Bye))
+        print("#A# Nb joueurs = {} \n#A# Bye = {}".format(n,Bye))
     if Berger==True: # choix méhode Berger ou ruban
         if extended==True: # Ajout liste inversée si besoin
             return methodeBerger(n)+reverseMatchlist(n)
@@ -28,7 +27,7 @@ def getMatchList(n,extended=False,Berger=False,debug_algo=False): # Fonction pri
 
 def methodeBerger(n): # Méthode utilisant règles de la table de Berger
     if debug:
-        print("#ALGO# Méthode Berger")
+        print("#A# Méthode Berger")
     if n%2!=0:
         return [] # Sécurité: vérifier si nombre joueurs est bien pair
 
@@ -57,7 +56,7 @@ def methodeBerger(n): # Méthode utilisant règles de la table de Berger
 
 def methodeRuban(n): # Méthode du ruban, plus rapide
     if debug:
-        print("#ALGO# Méthode ruban")
+        print("#A# Méthode ruban")
     if n%2!=0:
         return [] # Sécurité: vérifier si nombre joueurs est bien pair
     Rb=[i for i in range(2,n+1)] # générer ruban de joueurs sauf le 1
@@ -95,7 +94,7 @@ def duplicateMatch(p1,p2): # Fonction vérifiant les matchs doublons, matchlist 
 
 def reverseMatchlist(n): # Fonction inversant les 2 joueurs de chaque tuple-match de matchlist, pour le tournoi étendu
     if debug:
-        print("#ALGO# Ajout inversion matchlist")
+        print("#A# Ajout inversion matchlist")
     invlist=[(i[0]+n-1,i[2],i[1]) for i in matchlist]
     return invlist
 
@@ -103,55 +102,80 @@ def terme2(untuple): # Fonction retournant le 2e terme d'un tuple, utilisée com
     return untuple[1]
 def terme3(liste): # Fonction retournant le 2e terme d'un tuple, utilisée comme clé pour sort()
     return liste[2]
-def getClassement(n,matchlist,results,kw=1,kd=0,kl=0,debug=False): # Fonction de calcul des scores et du classement
+def getClassement(n,matchlist,results,scoremode=False,kw=1,kd=0,kl=0,debug=False): # Fonction de calcul des scores et du classement
     from copy import deepcopy
     classement=[]
-    scores=[]
+    scores=[[i,0] for i in range(1,n+1)]
     nbMatchs=len(matchlist)
     if debug:
-        print("#ALGO# Liste des matchs :\n",matchlist)
-        print("#ALGO# Résultats :\n",results)
-        print("#ALGO# Calcul score pour {} matchs".format(nbMatchs))
-    # win est la liste des gagnants (ex: win[4] donne le gagnant du 4e match)
+        print("#A# Liste des matchs :\n",matchlist)
+        if scoremode:
+            print("#A# Mode : Scores")
+        else:
+            print("#A# Mode : Tout Ou Rien")
+        print("#A# Résultats :\n",results)
+        for i in range (nbMatchs):
+            print("Ronde {} - Match {}\n                    {} VS {}\n         Résultat : {} à  {}".format(matchlist[i][0],i+1,matchlist[i][1],matchlist[i][2],results[i][0],results[i][1]))
+        print("#A# Calcul score pour {} matchs".format(nbMatchs))
     win=[[] for i in range(n)]
     draw=deepcopy(win)
     lose=deepcopy(win)
-    # On met l'id d'un joueur dans draw quand il fait une égalité
-    # et dans lose quand il perd
-    for i in range(0,nbMatchs):
 
-        # On a accès au gagnant avec results[i], donc on définit loser pour avoir accès au perdant
-        if results[i] == matchlist[i][1]: # Si le gagnant du match i est le joueur 1 du match i,
-            loser=matchlist[i][2] # le perdant est le joueur 2
-        elif results[i] == matchlist[i][2]: # sinon si le gagnant est le joueur 2,
-            loser=matchlist[i][1] # le perdant est le joueur 1
-        elif results[i] == 0: # sinon si le gagnant est 0 (égalité)
-            draw[matchlist[i][1]-1].append(matchlist[i][2])
-            draw[matchlist[i][2]-1].append(matchlist[i][1])
-            continue
+    if scoremode:
+        for i in range(nbMatchs):
+            scorej1=results[i][0]
+            scorej2=results[i][1]
+            scores[matchlist[i][1]-1][1]+=scorej1
+            scores[matchlist[i][2]-1][1]+=scorej2
+            if scorej1>scorej2:
+                winner=matchlist[i][1]
+                loser=matchlist[i][2]
+            elif scorej1<scorej2:
+                winner=matchlist[i][2]
+                loser=matchlist[i][1]
+            else:
+                draw[matchlist[i][1]-1].append(matchlist[i][2]) # pas de perdant, les deux vont dans la liste d'égalités de l'autre
+                draw[matchlist[i][2]-1].append(matchlist[i][1])
+                continue
+
+            win[winner-1].append(loser)
+            lose[loser-1].append(winner)
+        if debug:
+            print("#A# Scores : ",scores)
+    else:
+        for i in range(nbMatchs):
+            # On a accès au gagnant avec results[i], donc on définit loser pour avoir accès au perdant
+            if results[i] == matchlist[i][1]: # Si le gagnant du match i est le joueur 1 du match i,
+                loser=matchlist[i][2] # le perdant est le joueur 2
+            elif results[i] == matchlist[i][2]: # sinon si le gagnant est le joueur 2,
+                loser=matchlist[i][1] # le perdant est le joueur 1
+            elif results[i] == 0: # sinon si le gagnant est 0 (égalité)
+                draw[matchlist[i][1]-1].append(matchlist[i][2]) # pas de perdant, les deux vont dans la liste d'égalités de l'autre
+                draw[matchlist[i][2]-1].append(matchlist[i][1])
+                continue
         # Ici le gagnant est results[i], le perdant est loser
         win[results[i]-1].append(loser) # On ajoute le perdant à la liste de victoires du gagnant
         lose[loser-1].append(results[i]) # On ajoute le gagnant à la liste de défaites du perdant
     if debug:
-        print("#ALGO# Liste des victoires :\n",win)
-        print("#ALGO# Liste des égalités :\n",draw)
-        print("#ALGO# Liste des défaites \n",lose)
+        print("#A# Liste des victoires :\n",win)
+        print("#A# Liste des égalités :\n",draw)
+        print("#A# Liste des défaites \n",lose)
 
 
     for pl in range(1,n+1): #Calcul du score pour chaque joueur
         score = kw*len(win[pl-1]) + kd*len(draw[pl-1]) + kl*len(lose[pl-1])
-        scores.append([pl,score])
+        scores[pl-1][1]+=score
     for i in scores: # Une fois les scores obtenus, calcul du score Sonneborn-Berger pour chaque joueur
         i.append(getSonnBerg(i[0],scores,win,draw))
         if debug:
-            print("#ALGO# -- Joueur : {} | Score : {} | SonnBerg : {}".format(i[0],i[1],i[2]))
+            print("#A# -- Joueur : {} | Score : {} | SonnBerg : {}".format(i[0],i[1],i[2]))
     if debug:
-        print("#ALGO# Classement non ordonné :\n",scores)
+        print("#A# Classement non ordonné :\n",scores)
 
     classement=bubbleSort(scores,win) # Tri à bulles suivant les 3 critères
     classement.reverse() # On inverse pour avoir un classement décroissant
     if debug:
-        print("#ALGO# Classement ordonné :\n",classement)
+        print("#A# Classement ordonné :\n",classement)
     return classement
 
 def getSonnBerg(pl,scores,win,draw): # Fonction de calcul du score Sonneborn-Berger (SBscore)) pour le joueur pl
@@ -179,19 +203,21 @@ def bubbleSort(scores,win): # Fonction de tri "à bulles" de scores, le meilleur
     return scores
 
 
-def SimulateRandom(nbjoueurs,extended=False,berger=False,kwin=1,kdraw=0,klose=0):
-    from random import choice
+def SimulateRandom(nbjoueurs,extended=False,berger=False,scoremode=False,kwin=1,kdraw=0,klose=0):
+    from random import choice,randrange
     from time import time
     start_time=time()
     prev_time=time()
     test_matchlist=getMatchList(nbjoueurs,extended,berger,True)
     print('        getMatchList execution time : {} ms'.format((time()-prev_time)*1000))
 
-    test_results=[]
-    for i in test_matchlist:
-        test_results.append(choice([i[1],i[2],0]))
+    if scoremode:
+        test_results=[(randrange(0,4),randrange(0,4)) for i in test_matchlist]
+    else:
+        test_results=[choice([i[1],i[2],0]) for i in test_matchlist]
+
     prev_time=time()
-    getClassement(nbjoueurs,test_matchlist,test_results,kwin,kdraw,klose,True)
+    getClassement(nbjoueurs,test_matchlist,test_results,scoremode,kwin,kdraw,klose,True)
     print('        getClassement execution time : {} ms'.format((time()-prev_time)*1000))
 
 # Exécute getMatchList seulement si le programme est lancé seul (non importé)
